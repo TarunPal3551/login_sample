1a79caaf500687d880c862a4163ae48c8e3958bd
Tarun <tarunplay3551@gmail.com>
2020-07-18 12:18:31 +0530

    login_sample first commit

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: true,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+
+
+        children: <Widget>[
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.fromLTRB(20, 100, 0, 0),
+                  child: Text(
+                    "Hello",
+                    style:
+                        TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "There",
+                        style: TextStyle(
+                            fontSize: 80.0, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        ".",
+                        style: TextStyle(
+                            fontSize: 80.0, fontWeight: FontWeight.bold,color: Colors.green),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(20, 1000, 0, 0),
+            decoration: BoxDecoration(color: Colors.green),
+            child: Center(
+              child: Container(
+                child:TextFormField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Enter a search term'
+                  ),
+                ) ,
+              ),
+
+            ),
+          )
+        ],
+      ),
+      ),
+    );
+  }
+}
